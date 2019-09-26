418063e9a3e4eafbfe395cf32fb644074d2c6de8
Cam Webb <cw@camwebb.info>
2019-09-26 12:44:30 -0800

    auto

---
+++ tools/elisp/modes.el
@@ -0,0 +1,35 @@
+;; Modes
+
+;; 1. Open grammar file
+;; 2. C-x C-f  mode file
+;; 3. M-x eval-buffer
+;; 4. C-x b  to grammar
+;; 5. M-x grammar-mode (refreshes too)
+
+(define-generic-mode 'lexware-mode
+  'nil
+  'nil
+  '(
+    ("^#.*$" . 'font-lock-comment-face)
+    ("^\\.[^ .]+" . 'custom-face-tag)
+    ("^\\.\\.?\\.?[^ ]+" . 'font-lock-function-name-face)
+    ("^[^ ]+" . 'font-lock-keyword-face)
+    )
+  '(".*\\.lexware$" ".*\\.lw$")
+  nil
+  "Major mode for editing Lexware text")
+
+
+(define-generic-mode 'grammar-mode
+  '("#") ; comment char
+  '("(1-to-many)" "0-or-1" "0-to-many" "exactly-1" "1-or-more")
+  '(("^<[^>]*>" . 'font-lock-builtin-face)
+    ("^\\ *[^ ]+" . 'font-lock-function-name-face)
+    ("[|=]" . 'font-lock-constant-face))
+  '("\\.grammar$")
+  nil
+  "Major mode for editing grammars")
+
+
+;; M-x list-faces-display  to see faces
+
